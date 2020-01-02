15623c5f50c1680c505bc9a36a0ceaf444b2c04e
shehan <shehanp068@gmail.com>
2020-01-03 01:08:25 +0530

    firebase and others connect

---
+++ Ecommerce-Template-Bootstrap-master/firebase/init.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase'
+import firebase from 'firebase/auth'
+import firebase from 'firebase/firestore'
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDM595eUaA7jbvlrrCdi920LUOTa-30t80",
+    authDomain: "coffeeshop-4cf38.firebaseapp.com",
+    databaseURL: "https://coffeeshop-4cf38.firebaseio.com",
+    projectId: "coffeeshop-4cf38",
+    storageBucket: "coffeeshop-4cf38.appspot.com",
+    messagingSenderId: "966275901816",
+    appId: "1:966275901816:web:65ee85b80fc456d3ad008d"
+  };
+  // Initialize Firebase
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  firebaseApp.firestore().settings({timestampsInSnapshots:true})
+
